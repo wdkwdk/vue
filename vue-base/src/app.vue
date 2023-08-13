4097854e7b01d8e7da4d0e48333c5a1142c5c480
Twistzz <2031557931@qq.com>
2023-08-14 00:50:44 +0800

    vue

---
+++ vue-base/src/app.vue
@@ -0,0 +1,21 @@
+<template>
+    <p>{{ msg }}</p>
+    <p>{{ hello }}</p>
+    <p>{{ number+1 }}</p>
+    <p>{{ ok?'Yes':'No' }}</p>
+    <p v-html="rawHtml"></p>
+</template>
+<script>
+export default{
+    data(){
+        return{
+            msg:"神奇的语法",
+            hello:"Hello,World",
+            number:10,
+            ok:true,
+            rawHtml:"<a href='https://baidu.com'>baidu</a>"
+            
+        }
+    }
+}
+</script>
